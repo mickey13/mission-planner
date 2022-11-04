0fd2a3a93dc0ed304939b45de350397834d293f7
Mickey Cowden <mickey@cowden.tech>
2022-11-03 21:01:22 -0400

    Update resource properties for main form.

--- MainAFT.Designer.cs
+++ MainAFT.Designer.cs
@@ -28,10 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainAFT));
+            this.SuspendLayout();
+            // 
+            // MainAFT
+            // 
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MainAFT";
+            this.KeyPreview = true;
+            this.Name = "MainAFT";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
